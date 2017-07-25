ece6138fdd5df83cf1c544dc9f677d43ca18900d
derwentx <derwentx@gmail.com>
2017-07-25 10:40:35 +1000

    YCM install, merge install scripts

--- install.sh
+++ install.sh
@@ -1,22 +1,11 @@
 git submodule update --init --recursive
 
-cd ~/.vim
-
-echo 'set runtimepath+=~/.vim
-
-source ~/.vim/vimrcs/basic.vim
-source ~/.vim/vimrcs/filetypes.vim
-source ~/.vim/vimrcs/plugins_config.vim
-source ~/.vim/vimrcs/extended.vim
-
-try
-source ~/.vim/my_configs.vim
-catch
-endtry' > ~/.vimrc
+ln -s vimrc-awesome.vim ~/.vimrc
+ln -s . ~/.vim
 
 # Install YCM
 cd ~/.vim/bundle/YouCompleteMe
-brew install cmake go cargo mono rust
+brew install cmake go mono rust
 npm install -g typescript
 ./install.py --gocode-completer --clang-completer --tern-completer --racer-completer --omnisharp-completer
 
--- my_configs.vim
+++ my_configs.vim
@@ -1,6 +1,12 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-" Derwent Vim Config
+" Source other files
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+set runtimepath+=~/.vim
+
+source ~/.vim/vimrcs/basic.vim
+source ~/.vim/vimrcs/filetypes.vim
+source ~/.vim/vimrcs/plugins_config.vim
+source ~/.vim/vimrcs/extended.vim
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 " => Stuff from dotfiles repo
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
